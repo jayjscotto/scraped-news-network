af3a44015a401f10c49aee1dd549793ea35dcc5c
Jason <jayjscotto@gmail.com>
2020-01-04 16:26:40 -0500

    updated server files

--- server/app.js
+++ server/app.js
@@ -1,37 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const exphbs = require("express-handlebars");
-const path = require("path");
-
-
-const userRouter = require("./routes/user-routes");
-const apiRouter = require("./routes/api");
-const facilityRouter = require("./routes/facility-routes");
-
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const mongoose = require('mongoose');
+const router = require('./router');
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-//register moment helper
-exphbs.registerHelper("moment", require("helper-moment"));
-exphbs.registerHelper("formatDateTime", function(date, format) {
-  var date = moment(date, "H").format("h:mm a");
-  return date;
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+app.set('view options', { layout: 'layout' });
 
-app.set("view engine", "hbs");
-app.set("view options", { layout: "layout" });
-exphbs.registerPartials(__dirname + '/views/partials');
+mongoose.connect(
+  `mongodb+srv://jayjs:${process.env.DB_PASSWORD}@cluster0-uudkd.mongodb.net/scrapednewsnetwork?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+);
 
-// app.engine('hbs', engines.hbs);
 //middleware
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(express.static(path.join(__dirname, "/public")));
-
-app.use("/", router);
-
+app.use('/', router);
 
 module.exports = app;
