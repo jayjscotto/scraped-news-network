68de39b65e120026d23d14301e8894cc7342865b
Jason <jayjscotto@gmail.com>
2020-01-06 16:08:05 -0500

    added modal, updated routing

--- public/javascript/view_article.js
+++ public/javascript/view_article.js
@@ -1,1 +1,44 @@
 //script for users to navigate and click through different articles
+// Get the modal
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+
+  // Get the button that opens the modal
+  const btns = document.querySelectorAll('.modal-open').forEach(btn => {
+    // When the user clicks on the link button, open the modal
+    btn.addEventListener('click', e => {
+      const link = e.target.getAttribute('data-link');
+      const request = new XMLHttpRequest();
+      request.open('GET', '/article?' + link, true);
+      request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+          // Success!
+          modal.style.display = 'block;';
+        
+        } else {
+          // We reached our target server, but it returned an error
+        }
+      };
+      request.onerror = function() {
+        // There was a connection error of some sort
+      };
+      request.send();
+    });
+  });
+
+  // Get the <span> element that closes the modal
+  const span = document.getElementsByClassName('close')[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = 'none';
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+});
--- routes/router.js
+++ routes/router.js
@@ -5,13 +5,41 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 
 router.get('/', (req, res) => {
-  //do some cheerio and axios work
   //point axios at a url
   //use cheerio to scrape
   //put results in handlebars object
   //send to front end
 
-  axios.get('http://www.nytimes.com').then(response => {
+//   axios.get('http://www.nytimes.com').then(response => {
+//     //$ to load the response data with cheerio
+//     const $ = cheerio.load(response.data);
+
+//     //array for the articles that will be scraped
+//     const articles = [];
+
+//     //for each article
+//     $('article').each((i, element) => {
+//       //scrape the title
+//       const title = $(element)
+//         .children()
+//         .text();
+//       //scrape the link to the post
+//       const link = $(element)
+//         .find('a')
+//         .attr('href');
+//       //push the article title and link to the articles array
+//       articles.push({
+//         title: title,
+//         link: link
+//       });
+//     });
+
+//     //handlebars object
+//     const scraped = {
+//       articles: articles
+//     };
+
+axios.get('http://www.slate.com').then(response => {
     //$ to load the response data with cheerio
     const $ = cheerio.load(response.data);
 
@@ -19,10 +47,11 @@ router.get('/', (req, res) => {
     const articles = [];
 
     //for each article
-    $('article').each((i, element) => {
+    $('.story-teaser').each((i, element) => {
       //scrape the title
       const title = $(element)
-        .children()
+        .find('h3')
+        .attr('class', 'story-teaser__headline')
         .text();
       //scrape the link to the post
       const link = $(element)
@@ -42,6 +71,19 @@ router.get('/', (req, res) => {
     //console.log(articles);
     res.render('home', scraped);
   });
+});
+
+router.get('/article?:article', (req, res) => {
+  console.log(Object.keys(req.query)[0]);
+  //const article = req.params.article.split('').slice(9).join('')
+  
+//   axios.get('http://nytimes.com' + article).then(response => {
+//     //$ to load the response data with cheerio
+//     const $ = cheerio.load(response.data);
+
+//   });
+
+res.status(200).send()
 });
 
 module.exports = router;
