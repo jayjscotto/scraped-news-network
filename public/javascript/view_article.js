991ffc0d23227b9fc22190d0f0d15ac8e3fe8094
Jason <jayjscotto@gmail.com>
2020-01-08 14:37:32 -0500

    updated article fetch function

--- public/javascript/view_article.js
+++ public/javascript/view_article.js
@@ -1,63 +1,58 @@
-//script for users to navigate and click through different articles
-// Get the modal
+// array that will contain specific article
+const article = [];
+
+// async function to send the article link to the server
+// the server then scrapes the link at that route
+// the server then returns the scraped article
+// the object is turned into JSON, then pushed to the article array
+const getArticle = async link => {
+  article.splice(0, article.length);
+  const response = await fetch('/article?' + link);
+  const data = await response.json();
+  return article.push(data[0]);
+};
 
 document.addEventListener('DOMContentLoaded', () => {
+  // grabbing article modal
   const modal = document.getElementById('modal');
 
-  // Get the button that opens the modal
+  // Grab the button that opens the modal
   const btns = document.querySelectorAll('.modal-open').forEach(btn => {
-    // When the user clicks on the link button, open the modal
-
+    
     btn.addEventListener('click', e => {
       const link = e.target.getAttribute('data-link');
-     
+
       console.log('hello');
 
-      const options = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-
-      fetch('/article?' + link)
-        .then(response => {
+      getArticle(link).then(data => {
+        // after the article object is returned, show the modal
         modal.style.display = 'block';
-          return response.json()
-          
-        })
-        .then(response => {
-          console.log(typeof response[0].paragraphs);
-            const p = document.createElement('p')
-            const modalBody = document.querySelector('modal-body');
-            console.log(modalBody.textContent);
-            // p.textContent = response[0].title;
-            // //p.textContent = response[0].paragraphs, 
-            // modalBody.appendChild(p)
-          
-     
-            p.textContent = response[0].paragraphs
-            modalBody.appendChild(p)
-          
-
-        });
- 
-
+        
+        const modalBody = document.querySelector('.modal-body');
+        const header = document.querySelector('.modal-header h1');
+        const p = document.createElement('p');
+        
+        header.textContent = article[0].title;
+        p.textContent = article[0].paragraphs;
+        modalBody.appendChild(p);
+        
+        console.log(article[0].paragraphs);
+      });
     });
-  });
 
-  // Get the <span> element that closes the modal
-  const span = document.getElementsByClassName('close')[0];
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName('close')[0];
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = 'none';
-  };
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
       modal.style.display = 'none';
-    }
-  };
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    };
+  });
 });
