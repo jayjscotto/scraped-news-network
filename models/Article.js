958204ee5266e27b501fad7961770fa51f9fd9e3
jayjscotto <jayjscotto@gmail.com>
2020-01-08 22:49:13 -0500

    updated models and schema

---
+++ models/Article.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    link: {
+        type: String,
+        required: true
+    },
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+});
+
+const Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
--- routes/router.js
+++ routes/router.js
@@ -10,35 +10,6 @@ router.get('/', (req, res) => {
   //put results in handlebars object
   //send to front end
 
-  //   axios.get('http://www.nytimes.com').then(response => {
-  //     //$ to load the response data with cheerio
-  //     const $ = cheerio.load(response.data);
-
-  //     //array for the articles that will be scraped
-  //     const articles = [];
-
-  //     //for each article
-  //     $('article').each((i, element) => {
-  //       //scrape the title
-  //       const title = $(element)
-  //         .children()
-  //         .text();
-  //       //scrape the link to the post
-  //       const link = $(element)
-  //         .find('a')
-  //         .attr('href');
-  //       //push the article title and link to the articles array
-  //       articles.push({
-  //         title: title,
-  //         link: link
-  //       });
-  //     });
-
-  //     //handlebars object
-  //     const scraped = {
-  //       articles: articles
-  //     };
-
   axios.get('http://www.slate.com').then(response => {
     //$ to load the response data with cheerio
     const $ = cheerio.load(response.data);
